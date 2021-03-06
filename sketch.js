ea85973df11c14b48b7d6c35a1b73660e0b5df86
MalachiWill <mwilliams@my.harrisburgu.edu>
2021-03-05 19:52:40 -0500

    Songs added

--- sketch.js
+++ sketch.js
@@ -4,7 +4,8 @@ function preload() {
 song1 = loadSound("deja-vu.mp3");
 song2 = loadSound("run-vine-sound-effect.mp3");
 song3 = loadSound("ali-as-meme-intro-original-download.mp3")
-
+song4 = loadSound("03 Main Menu.mp3")
+song5 = loadSound("28 Super Buu.mp3")
 }
 function setup(){
      createCanvas(innerWidth,innerHeight);
@@ -24,6 +25,18 @@ function setup(){
      button = createButton('"Ali-a Intro" ');
      button.position(200, 550);
      button.mousePressed(songPlay3); 
+    
+     createCanvas(innerWidth,innerHeight);
+     background(200);
+     button = createButton('"Super Buu" ');
+     button.position(200, 575);
+     button.mousePressed(songPlay4);
+    
+    createCanvas(innerWidth,innerHeight);
+     background(200);
+     button = createButton(' "Main Menu" ');
+     button.position(200, 600);
+     button.mousePressed(songPlay5);
     
     }
 function draw() {
@@ -64,3 +77,29 @@ function songPlay3(){
           background('pink');
       }
 }
+
+}
+
+function songPlay4(){
+    if(song4.isPlaying()) {
+        song4.stop();
+        background('green');
+
+}
+        else{
+            song4.play();
+            background('red')
+        }
+}
+
+function songPlay5(){
+    if(song5.isPlaying()) {
+        song5.stop();
+        background('black');
+}
+        else{
+            song5.play();
+            background('pink')
+        }
+}
+                
